2c7b6273ae734cf89fe2b8d8ad10ef92b474e50d
Maneesh-Annamala <maneeshannamala@gmail.com>
2026-05-25 18:54:33 +0530

    Added Day 1 project

---
+++ Day-01/Simple_BrandName_Generator.py
@@ -0,0 +1,4 @@
+print("welcome to Brand name generator")
+city = input("what city that you grow up:\n ")
+pet = input("What is your pet name:\n ")
+print("your brand name would be" +  city+ " "+ pet)
